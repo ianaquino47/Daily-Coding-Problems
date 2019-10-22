83c9be88dc406fd92250ea14c364bd1ceb443fd2
iAquino47 <ianaquino47@gmail.com>
2019-10-22 18:22:03 +0100

    problem 10

---
+++ Problem_10.py
@@ -0,0 +1,24 @@
+# This problem was asked by Apple.
+
+# Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
+from time import sleep
+import threading
+
+class Scheduler:
+    def __init__(self):
+        self.fns = [] # tuple of (fn, time)
+        t = threading.Thread(target=self.poll)
+        t.start()
+
+    def poll(self):
+        while True:
+            now = time() * 1000
+            for fn, due in self.fns:
+                if now > due:
+                    fn()
+            self.fns = [(fn, due) for (fn, due) in self.fns if due > now]
+            sleep(0.01)
+
+    def delay(self, f, n):
+        self.fns.append((f, time() * 1000 + n))
