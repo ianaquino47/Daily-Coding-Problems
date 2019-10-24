9b2dbe49d2129ff9bcdd2dc389fb8f4822dbc65a
iAquino47 <ianaquino47@gmail.com>
2019-10-24 21:10:57 +0100

    problem 13

---
+++ Problem_13.py
@@ -0,0 +1,27 @@
+# This problem was asked by Amazon.
+
+# Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+# For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+def longest_substring_with_k_distinct_characters(s, k):
+    if k == 0:
+        return 0
+
+    # Keep a running window
+    bounds = (0, 0)
+    h = {}
+    max_length = 0
+    for i, char in enumerate(s):
+        h[char] = i
+        if len(h) <= k:
+            new_lower_bound = bounds[0] # lower bound remains the same
+        else:
+            # otherwise, pop last occurring char
+            key_to_pop = min(h, key=h.get)
+            new_lower_bound = h.pop(key_to_pop) + 1
+
+        bounds = (new_lower_bound, bounds[1] + 1)
+        max_length = max(max_length, bounds[1] - bounds[0])
+
+    return max_length
