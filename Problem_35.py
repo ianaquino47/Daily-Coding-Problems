388fe46d25710e871a458096282f5dd75536934b
iAquino47 <ianaquino47@gmail.com>
2019-11-29 20:09:09 +0000

    problem 35

---
+++ Problem_35.py
@@ -0,0 +1,20 @@
+# This problem was asked by Google.
+
+# Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
+
+# Do this in linear time and in-place.
+
+# For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'], it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
+
+def partition(arr):
+    low, mid, high = 0, 0, len(arr) - 1
+    while mid <= high:
+        if arr[mid] == 'R':
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 'G':
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
